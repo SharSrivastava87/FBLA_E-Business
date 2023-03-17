1d8f74aadb96b48bd3b20b07f6a0202ce838cf1f
SharSrivastava87 <123768339+SharSrivastava87@users.noreply.github.com>
2023-03-17 15:37:58 -0500

    9th working copy with Login(NotFunctional bc CSRF token)

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import BookingList, RoomList, BookingView
+from register.views import login_view
 from . import views
 app_name = 'Homepage'
 
@@ -8,6 +9,6 @@ urlpatterns=[
     path('NearbyAttractions/', BookingList.as_view(), name = 'BookingList'),
     path('book/', BookingView.as_view(), name='booking_view'),
     path('Pricing/', views.my_view),
-
+    path('login/', login_view, name='login'),
 
 ]
